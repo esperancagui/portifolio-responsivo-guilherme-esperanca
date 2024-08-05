06dac387c7b32a1ee82a77767d4f5bf35e16cf0c
Guilherme Esperança <129303960+esperancagui@users.noreply.github.com>
2024-08-05 11:04:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const linkBt = document.querySelectorAll('.link__botao')
-const imagem = document.querySelector('.imagem')
-const seletor = imagem.getAttribute('alt')
-
-linkBt.addEventListener('click', ()=>{
-    imagem.setAttribute('src', `img/${seletor}.png`)
-})
 
